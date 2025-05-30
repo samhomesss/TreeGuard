7e8c6a14dbadd87734fb3db8d28489cfc8e3b58f
Yoon.07.09 <159544864+samhomesss@users.noreply.github.com>
2025-05-30 16:43:52 +0900

    [Fix]_GIveWaterUI

--- Assets/@Project Scripts/UI/SkillTree/UI_SkillTree.cs
+++ Assets/@Project Scripts/UI/SkillTree/UI_SkillTree.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class UI_SkillTree : UI_Scene
 {
     Canvas _canvas;
-
+    UI_GiveWater _uiGiveWater;
     public override bool Init()
     {
         if (base.Init() == false)
             return false;
 
         _canvas = GetComponent<Canvas>();
+        _uiGiveWater = FindAnyObjectByType<UI_GiveWater>();
 
         Managers.Game.OnUISkillTreeCanvasEvent += CanvasOnOff;
 
@@ -19,5 +20,6 @@ public class UI_SkillTree : UI_Scene
     void CanvasOnOff()
     {
         _canvas.enabled = !_canvas.enabled;
+        _uiGiveWater.GetComponent<Canvas>().enabled = _canvas.enabled;
     }
 }
