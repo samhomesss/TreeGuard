b9ef8fc41d9330224883583772b7984a6f3a5c48
sayishuu <smile52673@gmail.com>
2025-05-30 16:44:44 +0900

    [Merge] 아이템 얻는거 ui적용

--- Assets/@Project Scripts/HSC/Map/GettableItem.cs
+++ Assets/@Project Scripts/HSC/Map/GettableItem.cs
@@ -7,6 +7,16 @@ public class GettableItem : MonoBehaviour, IInteractable
     public void Interact()
     {
         PlayerController.Instance.itemCount[itemType]++;
+
+        if (itemType == ItemType.Soul)
+        {
+            Managers.Game.GetItem1(1);
+        }
+        else if (itemType == ItemType.Leaf)
+        {
+            Managers.Game.GetItem2(1);
+        }
+
         Destroy(gameObject);
     }
 }
